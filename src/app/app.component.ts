79de1f4239744b0de121374257d9a5527ed672c6
juan david <juanvc123@hotmail.com>
2017-07-18 15:26:20 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+@Component({
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css']
+})
+
+export class AppComponent {
+	title = 'Tour of heroes';	
+	heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+}
+
+
+
+
+
