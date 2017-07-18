51772689e984d6b78ede192a9d60361000929468
juan david <juanvc123@hotmail.com>
2017-07-18 18:05:56 -0500

    avance del tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,19 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import {Component} from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: 'app.component.html',
-	styleUrls: ['app.component.css']
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>    
+        <nav>
+            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>  
+            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet> 
+    `,
+    styleUrls: ['app.component.css']
 })
 
 export class AppComponent {
-	title = 'Tour of heroes';	
-	heroes = HEROES;
-	selectedHero: Hero;
-
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
+    title = 'Tour of heroes';	
 }
-
-
-
-
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component'
+
+import { AppRoutingModule } from './app-routing.component';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeroDetailComponent,
+		HeroesComponent,
+		DashboardComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService),
+		FormsModule,
+		AppRoutingModule
+	],
+	providers: [HeroService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
+
