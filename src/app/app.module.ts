b3fdd9d1938339cb6fe13fb2f4f88cfcb082085d
juan david <juanvc123@hotmail.com>
2017-07-19 20:43:58 -0500

    organize files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './services/hero.service';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 import { AppRoutingModule } from './app-routing.component';
 // Imports for loading & configuring the in-memory web api
